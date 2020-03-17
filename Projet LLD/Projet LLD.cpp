650ef4e73963b876ee74bfc95f8d43d36eabe6d1
olivierSaintCyr <olivierSaintCyr@users.noreply.github.com>
2020-03-17 01:05:31 -0400

    xczxc

--- Projet LLD/Projet LLD.cpp
+++ Projet LLD/Projet LLD.cpp
@@ -5,23 +5,41 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <fstream>
 
 using namespace std;
 
 struct Numero {
 	string titre;
 	unsigned nDanseurs = 0;
-	Danseur* danseurs[500];
+	Danseur *ptrDanseurs[80];
 };
 
 struct Danseur {
 	string nom;
 	unsigned nNumeros = 0;
-	Numero* numero[30]; //hi
+	Numero *ptrNumeros[30];
 };
+
+void ajouterDanseur(Numero& numero, Danseur* listPtrDanseurs[]) {
+	
+}
+
+void lireFichier(string nomFichier) {
+	ifstream fichier(nomFichier);
+	if (fichier.fail()) {
+		cout << "Erreur lors de l'ouverture du fichier" << endl;
+	}
+	else {
+		while (!ws(fichier).eof()) {
+
+		}
+	}
+}
 
 int main()
 {
-    
+	string nomFichier = "Test_LLD";
+	
 }
 
