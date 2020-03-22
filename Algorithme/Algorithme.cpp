59a9f4c2bd7d155caaa92833dea6fc4dcf4ca36a
olivierSaintCyr <olivierSaintCyr@users.noreply.github.com>
2020-03-22 00:39:54 -0400

    asd

--- Algorithme/Algorithme.cpp
+++ Algorithme/Algorithme.cpp
@@ -10,97 +10,97 @@
 using namespace std;
 
 class Block {
-public:
-	unsigned start;
-	unsigned end;
-	unsigned deltaT;
-	Block(const unsigned Cstart, const unsigned Cend) {
-		start = Cstart;
-		end = Cend;
-		deltaT = Cend - Cstart;
-	}
+	public:
+		unsigned start;
+		unsigned end;
+		unsigned deltaT;
+		Block(const unsigned Cstart, const unsigned Cend) {
+			start = Cstart;
+			end = Cend;
+			deltaT = Cend - Cstart;
+		}
 };
 
 class PlageTemps : public Block {
-public:
-	unsigned nBlocks = 0;
-	Block* blocks[256];
-	PlageTemps(const unsigned Cstart, const unsigned Cend) : Block(Cstart, Cend) {
-		blocks[0] = new Block(Cstart, Cend);
-		nBlocks++;
-	}
-
-	void afficher() {
-		if (nBlocks > 1) {
-			sortStartOrder();
+	public:
+		unsigned nBlocks = 0;
+		Block* blocks[256];
+		PlageTemps(const unsigned Cstart, const unsigned Cend) : Block(Cstart, Cend) {
+			blocks[0] = new Block(Cstart, Cend);
+			nBlocks++;
 		}
-
-		for (unsigned i = 0; i < nBlocks; i++) {
-			cout << blocks[i]->start << endl << blocks[i]->end << endl;
-		}
-	}
-
-	bool removeBlock(const Block block) {
-		bool canRemoveBlock = false;
-
-		if ((block.deltaT <= deltaT) && (nBlocks > 0)) {
+	
+		void afficher() {
 			if (nBlocks > 1) {
-				sortDeltaTOrder();
+				sortStartOrder();
 			}
+	
 			for (unsigned i = 0; i < nBlocks; i++) {
-				if (block.deltaT < blocks[i]->deltaT) {
-					canRemoveBlock = true;
-					if (block.start > blocks[i]->start && block.end < blocks[i]->end) {
-						blocks[nBlocks] = new Block(block.end, blocks[i]->end);
-						blocks[i]->end = block.start;
-						nBlocks++;
-					}
-					if (block.start == blocks[i]->start) {
-						blocks[i]->start = block.end;
-					}
-					if (block.end == blocks[i]->end) {
-						blocks[i]->end = block.start;
-					}
-					break;
+				cout << blocks[i]->start << endl << blocks[i]->end << endl;
+			}
+		}
+	
+		bool removeBlock(const Block block) {
+			bool canRemoveBlock = false;
+	
+			if ((block.deltaT <= deltaT) && (nBlocks > 0)) {
+				if (nBlocks > 1) {
+					sortDeltaTOrder();
 				}
-
-				if (block.deltaT == blocks[i]->deltaT) {
-					canRemoveBlock = true;
-					delete blocks[i];
-					nBlocks--;
-					break;
+				for (unsigned i = 0; i < nBlocks; i++) {
+					if (block.deltaT < blocks[i]->deltaT) {
+						canRemoveBlock = true;
+						if (block.start > blocks[i]->start && block.end < blocks[i]->end) {
+							blocks[nBlocks] = new Block(block.end, blocks[i]->end);
+							blocks[i]->end = block.start;
+							nBlocks++;
+						}
+						if (block.start == blocks[i]->start) {
+							blocks[i]->start = block.end;
+						}
+						if (block.end == blocks[i]->end) {
+							blocks[i]->end = block.start;
+						}
+						break;
+					}
+	
+					if (block.deltaT == blocks[i]->deltaT) {
+						canRemoveBlock = true;
+						delete blocks[i];
+						nBlocks--;
+						break;
+					}
+				}
+			}
+			return canRemoveBlock;
+		}
+	
+	private:
+		void sortStartOrder() {
+			for (unsigned i = 0; i < nBlocks; i++) {
+				for (unsigned j = 0; j < nBlocks - 1 - i; j++) {
+					if (blocks[j]->start > blocks[j + 1]->start) {
+						Block* a = blocks[j];
+						Block* b = blocks[j + 1];
+						blocks[j] = b;
+						blocks[j + 1] = a;;
+					}
 				}
 			}
 		}
-		return canRemoveBlock;
-	}
-
-private:
-	void sortStartOrder() {
-		for (unsigned i = 0; i < nBlocks; i++) {
-			for (unsigned j = 0; j < nBlocks - 1 - i; j++) {
-				if (blocks[j]->start > blocks[j + 1]->start) {
-					Block* a = blocks[j];
-					Block* b = blocks[j + 1];
-					blocks[j] = b;
-					blocks[j + 1] = a;;
+	
+		void sortDeltaTOrder() {
+			for (unsigned i = 0; i < nBlocks; i++) {
+				for (unsigned j = 0; j < nBlocks - 1 - i; j++) {
+					if (blocks[j]->deltaT > blocks[j + 1]->deltaT) {
+						Block* a = blocks[j];
+						Block* b = blocks[j + 1];
+						blocks[j] = b;
+						blocks[j + 1] = a;;
+					}
 				}
 			}
 		}
-	}
-
-	void sortDeltaTOrder() {
-		for (unsigned i = 0; i < nBlocks; i++) {
-			for (unsigned j = 0; j < nBlocks - 1 - i; j++) {
-				if (blocks[j]->deltaT > blocks[j + 1]->deltaT) {
-					Block* a = blocks[j];
-					Block* b = blocks[j + 1];
-					blocks[j] = b;
-					blocks[j + 1] = a;;
-				}
-			}
-		}
-	}
 };
 
 
